8983323d86b250c8d3de55c3f1ced1b5a832b863
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-10-11 23:17:25 +0300

    Update 1stprogramm.cpp

--- 1stprogramm.cpp
+++ 1stprogramm.cpp
@@ -2,18 +2,13 @@
 #include<math.h>
 #include<stdlib.h>
 using namespace std;
-//Вычислить значение a
-//Вывести на экран значение a
 void main()
 {
-	double x, y, a;// Определить переменные
-	// Ввести исходные данные
-	printf_s("ââåäèòå çíà÷åíèå x:");
+	double x, y, a;
+	printf_s("input x:");
 	scanf_s("%lf", &x);
-	printf_s("ââåäèòå çíà÷åíèå y:");
+	printf_s("input y:");
 	scanf_s("%lf", &y);
-	// Вычислить значание выражения
 	a = (sqrt(x*x + y*y) / (x*y - 2));
 	printf_s(" a=%f", a);
-	system("pause");// Остановить программу до нажатия любой клавиши
 }
