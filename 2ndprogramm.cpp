ec5e0806168a90168109647230041d5a55a22e7c
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-10-20 21:06:35 +0300

    Update 2ndprogramm.cpp

--- 2ndprogramm.cpp
+++ 2ndprogramm.cpp
@@ -1,45 +1,39 @@
-#include<iostream>
 #include<stdio.h>
+#include<iostream>
 #include<stdlib.h>
+#include<math.h>
 using namespace std;
-void readonlinebook()
-{
-	cout << "Read online book called";
-}
-void downloadonlinebook()
-{
-	cout << "Download online book called";
-}
-void listenonlinebook()
-{
-	cout << "Listen online book called";
-}
 int main()
 {
-	int input;
-
-
-	cout << "1. Read online\n";
-	cout << "2. Download online\n";
-	cout << "3. Listen online\n";
-	cout << "4. Exit\n";
-	cin >> input;
-	switch (input) {
-	case 1:
-		readonlinebook();
-		break;
-	case 2:
-		downloadonlinebook();
-		break;
-	case 3:
-		listenonlinebook();
-		break;
-	case 4:
-		cout << "Thank you for reading books!\n";
-		break;
-	default:
-		cout << "Error, bad input,quitting\n";
-	}
+int input;
+cout << "1.Read online\n";
+cout << "2.Download online\n";
+cout << "3.Listen online\n";
+cout << "4.Exit online\n";
+cin >> input;
+switch (input) {
+case 1: {
+	cout << "Read online book called\n";
+	break;
 }
+case 2: {
+	cout << "Download online book called\n";
+	break;
+}
+case 3: {
+	cout << "Listen online book called\n";
+	break;
+}
+case 4: {
+	cout << "Thank you for reading books!\n";
+	break;
+}
+default:
+	cout << "Error,bad input,quitting\n";
+}
+}
+
+
+
 
 	
